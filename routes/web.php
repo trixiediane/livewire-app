79e1cee757bdd5ad9f32699137ac9036c867ca40
trixiediane <swiftsklossy490@gmail.com>
2024-07-11 09:42:10 +0800

    added maryui tailwind

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,17 @@
 <?php
 
-use App\Livewire\Pages\Dashboard;
+use App\Livewire\Welcome;
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
 
-Route::get('/dashboard', Dashboard::class)->name('dashboard');
+Route::get('/', Welcome::class);
